5f23f9c2931722cca5e5a6a4f49de28902c04e27
Scott Corgan <scottcorgan@gmail.com>
2015-01-09 10:29:06 -0800

    Make it sow

---
+++ index.js
@@ -0,0 +1,50 @@
+var _ = require('lodash');
+var clearRequire = require('clear-require');
+
+module.exports = function (data) {
+  
+  // Return object passed in
+  if (_.isObject(data) && !Array.isArray(data)) {
+    return data;
+  }
+  
+  var config = file(data);
+  
+  return config;
+};
+
+function file (filename) {
+  
+  var config = {};
+  
+  // Load file
+  try {
+    
+    // Handle config file name
+    if (_.isString(filename)) {
+      config = load(filename);
+    }
+    
+    // Handle array of config file names as strings
+    if (Array.isArray(filename)) {
+      
+      filename = _.find(filename, function (name) {
+        
+        return _.isString(name) && fs.existsSync(name);
+      });
+      
+      if (filename) {
+        config = load(filename);
+      }
+    }
+  }
+  catch (e) {}
+  
+  return config;
+}
+
+function load (filename) {
+  
+  clearRequire(filename);
+  return require(filename);
+}
---
+++ test.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+var test = require('tape');
+
+var fs = require('fs-extra');
+
+var jfig = require('./index');
+
+test('filename string', function (t) {
+  
+  fs.outputFileSync('.config.json', '{"key":"value"}');
+  
+  var config =  jfig(path.join(process.cwd(), '.config.json'));
+  
+  t.deepEqual(config, {key: 'value'}, 'config');
+  
+  fs.removeSync('.config.json');
+  t.end();
+});
+
+test('object', function (t) {
+  
+  var config = jfig({
+    type: 'object'
+  });
+  
+  t.deepEqual(config, {type: 'object'}, 'object config')
+  t.end();
+});
+
+test('empty object for missing file', function (t) {
+  
+  var config =  jfig(path.join(process.cwd(), '.nope.json'));
+  
+  t.deepEqual(config, {}, 'empty');
+  t.end();
+});
